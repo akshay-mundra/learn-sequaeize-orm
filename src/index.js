65d705030c862b096b1c03f72047835af2a61efc
Akshay Mundra <akshay@gkmit.co>
2024-10-17 12:54:10 +0530

    feat: add express server setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(3000, () => {
+	console.log(`Server running on port : ${PORT} `);
+});
