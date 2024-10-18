8e828c236678e6a3d43ab9c38ffef2ed1c6845f5
Akshay Mundra <akshay@gkmit.co>
2024-10-18 10:34:48 +0530

    fix: model name phoneNumber to PhoneNumber

--- src/models/phonenumber.js
+++ src/models/phonenumber.js
@@ -48,7 +48,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'phoneNumber',
+      modelName: 'PhoneNumber',
       tableName: 'phone_numbers',
     },
   );
